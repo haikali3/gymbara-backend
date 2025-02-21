d71d66bd21b17eb9e1259076cb1e175eccfa370b
Haikal Tahar <manfdvcL9@gmail.com>
2025-02-22 00:57:12 +0800

    feat: refactor CreateSubscription function to validate environment variables and remove customer creation logic

--- internal/payment/customer-checkout.go
+++ internal/payment/customer-checkout.go
@@ -8,7 +8,6 @@ import (
 	"github.com/haikali3/gymbara-backend/pkg/utils"
 	"github.com/stripe/stripe-go/v81"
 	"github.com/stripe/stripe-go/v81/checkout/session"
-	"github.com/stripe/stripe-go/v81/customer"
 	"go.uber.org/zap"
 )
 
@@ -33,28 +32,44 @@ type SubscriptionRequest struct {
 }
 
 func CreateSubscription(w http.ResponseWriter, r *http.Request) {
+	// Set the Stripe API Key before calling Stripe
+	stripeKey := os.Getenv("STRIPE_SECRET_KEY")
+	// Stripe price ID (set this in your Stripe dashboard) > Product Catalog(Sidebar) > Triple Dot > PriceID
+	priceID := os.Getenv("STRIPE_PRICE_ID")
+	frontendURL := os.Getenv("FRONTEND_URL")
+
+	// Validate required environment variables
+	if stripeKey == "" || priceID == "" || frontendURL == "" {
+		utils.Logger.Error("Missing required environment variables", zap.String("StripeKey", stripeKey), zap.String("PriceID", priceID))
+		http.Error(w, "Stripe API Key, Price ID, or Frontend URL is missing", http.StatusInternalServerError)
+		return
+	}
+
+	// Set Stripe API key
+	stripe.Key = stripeKey
+
+	utils.Logger.Debug("Using Stripe API Key", zap.String("StripeKey", stripeKey))
+
 	var req SubscriptionRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
 
+	// ! stripe only able to show u customer or customer email only. so lets try email after this
 	// Create the customer explicitly
-	custParams := &stripe.CustomerParams{
-		Email: stripe.String(req.Email),
-	}
-	cust, err := customer.New(custParams)
-	if err != nil {
-		utils.Logger.Error("Failed to create Stripe customer", zap.Error(err))
-		http.Error(w, "Could not create customer", http.StatusInternalServerError)
-		return
-	}
+	// custParams := &stripe.CustomerParams{
+	// 	Email: stripe.String(req.Email),
+	// }
+	// cust, err := customer.New(custParams)
+	// if err != nil {
+	// 	utils.Logger.Error("Failed to create Stripe customer", zap.Error(err))
+	// 	http.Error(w, "Could not create customer", http.StatusInternalServerError)
+	// 	return
+	// }
 
-	// Stripe price ID (set this in your Stripe dashboard) > Product Catalog(Sidebar) > Triple Dot > PriceID
-	priceID := os.Getenv("STRIPE_PRICE_ID")
-	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
 	params := &stripe.CheckoutSessionParams{
-		Customer:           stripe.String(cust.ID),
+		// Customer:           stripe.String(cust.ID),
 		PaymentMethodTypes: stripe.StringSlice([]string{"card"}),
 		CustomerEmail:      stripe.String(req.Email),
 		LineItems: []*stripe.CheckoutSessionLineItemParams{
