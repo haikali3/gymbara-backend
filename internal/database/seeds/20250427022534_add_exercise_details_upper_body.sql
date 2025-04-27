780cb2ff863348dbd1d8739e9a5058f4634886b0
Haikal Tahar <manfdvcL9@gmail.com>
2025-04-27 18:41:39 +0800

    fix: update exercise details to remove specific load values for clarity

--- internal/database/seeds/20250427022534_add_exercise_details_upper_body.sql
+++ internal/database/seeds/20250427022534_add_exercise_details_upper_body.sql
@@ -1,4 +1,4 @@
--- file: internal/database/seeds/20250427022316_add_upper_body_exercise_details.sql
+-- file: internal/database/seeds/20250427022534_add_exercise_details_upper_body.sql
 -- +goose Up
 -- +goose StatementBegin
 INSERT INTO ExerciseDetails (
@@ -13,17 +13,17 @@ INSERT INTO ExerciseDetails (
   rest_time
 ) VALUES
   -- 2-Grip Pullup
-  (9, 5, 8, 1, 2, '8-10', '63kgx10', '9-10', '~3 MINS'),
+  (9, 5, 8, 1, 2, '8-10', NULL, '9-10', '~3 MINS'),
   -- Weighted Dip (Heavy)
-  (10, 5, 8, 2, 1, '6-8', '72kg x 6', '8-9', '~3 MINS'),
+  (10, 5, 8, 2, 1, '6-8', NULL, '8-9', '~3 MINS'),
   -- Weighted Dip (Back off)
-  (11, 5, 8, 0, 1, '10-12', '59kg x 10', '9-10', '~3 MINS'),
+  (11, 5, 8, 0, 1, '10-12', NULL, '9-10', '~3 MINS'),
   -- Incline Chest-Supported DB Row
-  (12, 5, 8, 1, 2, '8-10', '59kgx10', '9-10', '~2 MINS'),
+  (12, 5, 8, 1, 2, '8-10', NULL, '9-10', '~2 MINS'),
   -- Standing DB Arnold Press
-  (13, 5, 8, 1, 2, '8-10', '50kgx10', '9-10', '~2 MINS'),
+  (13, 5, 8, 1, 2, '8-10', NULL, '9-10', '~2 MINS'),
   -- A1: DB Incline Curl
-  (14, 5, 8, 1, 2, '15-20', '12.5kgx15', '10', '0 MINS'),
+  (14, 5, 8, 1, 2, '15-20', NULL, '10', '0 MINS'),
   -- A2: DB French Press
   (15, 5, 8, 1, 2, '15-20', NULL, '10', '~1.5 MINS');
 -- +goose StatementEnd
